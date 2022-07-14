3cd98142be1cd77f8550958194add1a2dab171de
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-07-14 15:43:07 +0600

    1

--- PrintAsync/Program.cs
+++ PrintAsync/Program.cs
@@ -59,3 +59,33 @@ async Task PrintAsync2(string message)
     await Task.Delay(new Random().Next(1000, 2000));     // имитация продолжительной операции
     Console.WriteLine(message);
 }
+
+var task22 = PrintAsync22("Hello Metanit.com"); // задача начинает выполняться
+Console.WriteLine("Main Works");
+
+await task22; // ожидаем завершения задачи
+
+// определение асинхронного метода
+async Task PrintAsync22(string message)
+{
+    await Task.Delay(0);
+    Console.WriteLine(message);
+}
+Console.WriteLine();
+
+var square5 = SquareAsync(5);
+var square6 = SquareAsync(6);
+
+Console.WriteLine("Остальные действия в методе Main");
+
+int n1 = await square5;
+int n2 = await square6;
+Console.WriteLine($"n1={n1}  n2={n2}"); // n1=25  n2=36
+
+async Task<int> SquareAsync(int n)
+{
+    await Task.Delay(0);
+    var result = n * n;
+    Console.WriteLine($"Квадрат числа {n} равен {result}");
+    return result;
+}
